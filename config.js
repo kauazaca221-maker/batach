9754af0e8026a71494be397a31a29fed173b4730
kauazaca221-maker <kauazaca221@gmail.com>
2026-07-07 18:08:51 -0300

    Atualiza webhook da landing page

--- config.js
+++ config.js
@@ -1,1 +1,1 @@
-window.BATACH_N8N_WEBHOOK_URL = "https://webhook.4scale.com.br/webhook/cea72bcb-58f9-4b21-b335-d7ec5fa5db06";
+window.BATACH_N8N_WEBHOOK_URL = "https://webhook.4scale.com.br/webhook/lead-landing-page";
